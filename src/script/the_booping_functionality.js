ca408e4f01c641f00524e0269a7e42ac87c84196
Gabriel Beltran <gabrielaudbeltran23@gmail.com>
2022-08-28 23:48:17 -0600

    Fourth commit that allows images to be preloaded already into the website so that slower networks can process it a tad bit better.

--- src/script/the_booping_functionality.js
+++ src/script/the_booping_functionality.js
@@ -5,6 +5,16 @@ $(function () {
     const anyaThudSound = new Audio("src/music/anya_thud.mp3");
     const bgSound = new Audio("src/music/Spy x Family Opening _ Official HIGE DANdism – Mixed Nuts (Instrumental).mp3");
 
+    /**
+     * Allows image to be already preloaded.
+     * @type {HTMLImageElement}
+     */
+    const boop1 = new Image();
+    const boop2 = new Image();
+    boop1.src = "src/img/boop1.png";
+    boop2.src = "src/img/boop2.png";
+    theBoopZone.attr('src', boop1.src);
+
     let thudcounter = 0;
 
     theBoopZone.on('mousedown touchstart', function ()
@@ -21,15 +31,17 @@ $(function () {
         {
             if (thudcounter >= 1)
             {
-
+                /**
+                 * Basically this doesnt allows thudcounter to go up more.
+                 */
             }
         }
 
-        $(this).attr('src', 'src/img/boop2.png');
+        $(this).attr('src', boop2.src);
         anyaCrySound.play();
 
     }).on('mouseup touchend', function () {
-        $(this).attr('src', 'src/img/boop1.png');
+        $(this).attr('src', boop1.src);
         anyaThudSound.play();
     });
 });
