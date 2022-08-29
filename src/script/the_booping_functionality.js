45d9eada9212a8e8ebb48a44a54d78d933c6e60a
Gabriel Beltran <gabrielaudbeltran23@gmail.com>
2022-08-29 00:07:32 -0600

    Fifth commit that adds new custom cursor

--- src/script/the_booping_functionality.js
+++ src/script/the_booping_functionality.js
@@ -4,6 +4,7 @@ $(function () {
     const anyaCrySound = new Audio("src/music/anya_cry.mp3");
     const anyaThudSound = new Audio("src/music/anya_thud.mp3");
     const bgSound = new Audio("src/music/Spy x Family Opening _ Official HIGE DANdism – Mixed Nuts (Instrumental).mp3");
+    const pointer = $("#pointer");
 
     /**
      * Allows image to be already preloaded.
@@ -14,8 +15,18 @@ $(function () {
     boop1.src = "src/img/boop1.png";
     boop2.src = "src/img/boop2.png";
     theBoopZone.attr('src', boop1.src);
-
+    
     let thudcounter = 0;
+
+    /**
+     * This method basically allows the pointer image to follow the now-hidden cursor
+     */
+    $(this).on('mousemove', function (x) {
+        pointer.css({
+            left: x.pageX + 5,
+            top: x.pageY + 5
+        });
+    })
 
     theBoopZone.on('mousedown touchstart', function ()
     {
@@ -39,9 +50,11 @@ $(function () {
 
         $(this).attr('src', boop2.src);
         anyaCrySound.play();
+        pointer.attr('src', 'src/img/pointer_clicked.png');
 
     }).on('mouseup touchend', function () {
         $(this).attr('src', boop1.src);
         anyaThudSound.play();
+        pointer.attr('src', 'src/img/pointer_not_clicked.png');
     });
 });
